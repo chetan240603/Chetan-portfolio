25b5a0e49897c78b6495f958e6a48a83a7471c49
Chetan Kumawat <chetanck2003@gmail.com>
2024-10-08 13:49:03 +0530

    new files added

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,50 +1,33 @@
 // --------------------------------------- Project List
 export const ProjectList = [
   {
-    img: "/ykte-feature-graphic.png",
-    title: "YouKnowTheEnding",
+    img: "/portfolio.png",
+    title: "Portfolio",
     description:
-      "A cross-platform mobile game (in closed testing) where players try to find the missing pieces of a story.",
-    tech_stack: ["React Native", "Google Cloud", "NextJs", "TailwindCSS"],
-    github_url: "https://github.com/chetan240603/",
+      " A personal portfolio website using HTML, CSS, and JavaScript to showcase projects and professional achievements.",
+    tech_stack: ["React ", "Tailwind CSS", "NodeJS", "Express"],
+    github_url: "https://github.com/chetan240603/Full_Stack_TODO_Application",
     demo_url: "",
   },
   {
-    img: "/renr-responsive.png",
-    title: "RENR Safety App",
+    img: "/realestate.png",
+    title: "Real Estate Management",
     description:
-      "A safety application for the Renewable Resources Dept. of BCIT",
-    tech_stack: ["React", "Firebase"],
-    github_url: "https://github.com/chetan240603/",
+      "An intuitive user interface using React.js, ensuring a seamless user experience for property browsing and management.",
+    tech_stack: ["React", "JavaScript","CSS", "Express"],
+    github_url: "https://github.com/chetan240603/Real_Estate_Frontend",
     demo_url: "",
   },
   {
-    img: "/mymind-responsive.png",
-    title: "MyMind",
+    img: "/todoapp.png",
+    title: "TODO Web App",
     description:
-      "An app where users can purchase online therapy sessions and chat with their therapists.",
-    tech_stack: ["HTML", "CSS", "NodeJs", "MongoDB", "SocketIo"],
-    github_url: "https://github.com/chetan240603/",
+      "A To-Do web application to help users manage tasks efficiently",
+    tech_stack: ["HTML", "CSS", "NodeJs", "MongoDB", "JavaScript"],
+    github_url: "https://github.com/chetan240603/Full_Stack_TODO_Application",
     demo_url: "",
   },
-  {
-    img: "/pokedex-responsive.png",
-    title: "Pokedex",
-    description:
-      "A pokedex app that displays pokemon data, plays OSTs and makes pokemons dance.",
-    tech_stack: ["ReactJs", "NodeJs", "MongoDB"],
-    github_url: "https://github.com/chetan240603/",
-    demo_url: "",
-  },
-  {
-    img: "/jobDetect.png",
-    title: "JobDetect",
-    description:
-      "An app to detect if a job posting is a scam or not using its description.",
-    tech_stack: ["Python", "Flask", "NodeJs", "MongoDB"],
-    github_url: "https://github.com/chetan240603/",
-    demo_url: "",
-  },
+  
 ];
 
 // --------------------------------------- Skills
@@ -70,54 +53,32 @@ export const stackList = [
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
     name: "NextJs",
   },
-  {
-    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
-    name: "Typescript",
-  },
+  
   {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
     name: "Git",
   },
-  {
-    img: "/icons8-docker.svg",
-    name: "Docker",
-  },
+ 
   {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
     name: "NodeJS",
   },
-  {
-    img: "/icons8-aws.svg",
-    name: "AWS",
-  },
+  
   {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
     name: "MongoDB",
   },
-  {
-    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
-    name: "Python",
-  },
+  
   {
     img: "/icons8-postgresql (1).svg",
     name: "PostGreSQL",
   },
-  {
-    img: "/icons8-prisma-orm.svg",
-    name: "Prisma",
-  },
+  
   {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
     name: "Bootstrap",
   },
-  {
-    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
-    name: "Material UI",
-  },
-  {
-    img: "/tailwind-css-svgrepo-com.svg",
-    name: "TailwindCSS",
-  },
+  
   {
     img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
     name: "VS Code",
